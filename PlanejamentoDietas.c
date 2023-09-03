25d9ac6d5c296dc5eb5c965469336392d07a89d4
Gustavo-DSC <gustavodouglas646@gmail.com>
2023-09-03 19:40:44 -0300

    Adicionada tela de criação de cardápio

--- PlanejamentoDietas.c
+++ PlanejamentoDietas.c
@@ -277,6 +277,43 @@ void menu_plan_alimentar(void){
     getchar();
 }
 
+void tela_cria_cardapio(void){
+    char ops;
+    system("clear||cls");
+    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
+    printf("||                                                                                                         ||\n");    
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||     ================================     CRIAÇÃO DE CARDÁPIOS    ==================================     ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||     Este cardápio foi crido, utilizando informações sobre sua altura, peso, idade e nível de atividade  ||\n");
+    printf("||     física para calcular suas necessidades calóricas diárias e criar um cardápio personalizado que      ||\n");
+    printf("||     atenda às suas necessidades nutricionais.                                                           ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||     Café da manhã:                                                                                      ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||     Almoço:                                                                                             ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||     Jantar :                                                                                            ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("||                                                                                                         ||\n");    
+    printf("||    0 [Voltar]                                                                                           ||\n");
+    printf("||                                                                                                         ||\n");
+    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
+    printf("||Selecione a opção:\n");
+    scanf("%c", &ops);
+    getchar();
+    
+}
+
 void menu_acompanhamento (void){
     char ops;
     system("clear||cls");
