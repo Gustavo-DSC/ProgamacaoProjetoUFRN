db060b9dbe57bcb2b8241914ce8f575c475e94b4
Gustavo-DSC <gustavodouglas646@gmail.com>
2023-09-17 16:31:53 -0300

    Modularização do menu de avaliação nutricional

--- PlanejamentoDietas.c
+++ PlanejamentoDietas.c
@@ -11,12 +11,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "usuario.h"
+#include "avaliacao_nutricional.h"
+
 
 char menu_inicial();
-char menu_avaliacao_nutri();
-char tela_imc ();
-char tela_ingestao_alimentar ();
 char menu_plan_alimentar();
 char tela_cria_cardapio();
 char tela_suges_substituicao();
@@ -33,20 +33,7 @@ int main(void){
     switch(opc){
       case '1': modulo_usuario();
         break;
-      case '2':
-        do {
-          opc_2 = menu_avaliacao_nutri();
-          switch(opc_2){
-            case '1':
-              tela_imc ();
-              break;
-            case '2':
-              tela_ingestao_alimentar ();
-              break;
-            case '0':
-              break;
-          }
-        } while (opc_2 != '0');
+      case '2': modulo_avaliacao_nutri(); 
         break;
       case '3':
         do {
@@ -125,94 +112,6 @@ char menu_inicial (){
 }
 
 
-
-char menu_avaliacao_nutri (){
-    char opc;
-    system("clear||cls");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||     ================================    AVALIAÇÃO  NUTRICIONAL   ==================================     ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                               1 [Cálculo do IMC]                                                        ||\n");
-    printf("||                               2 [Avaliação da Ingestão Alimentar]                                       ||\n");
-    printf("||                               0 [Voltar]                                                                ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||Selecione a opção:\n");
-    scanf(" %c", &opc);
-    return opc;
-}
-char tela_imc (){
-    char opc;
-    system("clear||cls");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||     ========================================   IMC USÚARIO  =======================================     ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||         tabela de valores de IMC          | INFORMAÇÕES                                                 ||\n");
-    printf("||     IMC               CLASSIFICAÇÃO       | O IMC (índice de Massa Corporal). É uma técnica utilizada   ||\n");
-    printf("||     < 16,00           Magreza grau III    | para verificar o estado nutricional e observar se a pessoa  ||\n");
-    printf("||     16,00 - 16,99     Magreza grau II     | está dentro dos padrões de normalidade com relação ao seu   ||\n");
-    printf("||     17,00 - 18,49     Magreza grau I      | peso e estatura                                             ||\n");
-    printf("||     18,50 - 24,99     Peso normal         |                                                             ||\n");
-    printf("||     25,00 - 29,99     Sobrepeso           | SEU IMC :                                                   ||\n");    
-    printf("||     30,00 - 34,99     Obesidade grau I    |                                                             ||\n");
-    printf("||     35,00 - 39,99     Obesidade grau II   |                                                             ||\n");
-    printf("||     ? 40,00           Obesidade grau III  |                                                             ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||     0 -[voltar]                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||Selecione a opção:\n");
-    scanf(" %c", &opc);
-    return opc;
-}
-char tela_ingestao_alimentar (){
-    char opc;
-    system("clear||cls");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||     =============================   AVALIAÇÃO DA INGESTÃO ALIMENTAR   =============================     ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||      INFORME SE DEIXOU DE FAZER UMA REFEIÇÃO DA DIETA                                                   ||\n");
-    printf("||          1 - café da manhã                                                                              ||\n");
-    printf("||          2 - almoço                                                                                     ||\n");
-    printf("||          3 - jantar                                                                                     ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||      INFORME COMO VOCÊ ESTÁ SE SENTINDO COM A ALIMENTAÇÃO                                               ||\n");
-    printf("||          1 - bem                                                                                        |\n");    
-    printf("||          2 - normal                                                                                     ||\n");
-    printf("||          3 - ruim                                                                                       ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||      INFOEME SE ALGUM ALIMENTO ESTÁ FAZENDO MAL                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||          0 [Voltar]                                                                                     ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||Selecione a opção:\n");
-    scanf(" %c", &opc);
-    return opc;
-}
 
 char menu_plan_alimentar(){
     char opc;
---
+++ avaliacao_nutricional.h
@@ -0,0 +1,4 @@
+char menu_avaliacao_nutri();
+char tela_imc ();
+char tela_ingestao_alimentar ();
+void modulo_avaliacao_nutri();
