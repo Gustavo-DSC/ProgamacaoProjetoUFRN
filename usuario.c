160f2f56fb263ce8e895dee968ab4b7b9d758420
Gustavo-DSC <gustavodouglas646@gmail.com>
2023-10-08 09:53:45 -0300

    Adiciona os campos de entrada no cadastrar usuário

--- usuario.c
+++ usuario.c
@@ -12,6 +12,7 @@ char sexo[100];
 char email[100];
 float peso;
 float altura;
+char ativ_fisica[100];
 
 
 // funções modulo usuario
@@ -132,32 +133,79 @@ char tela_restricao_alimentar (){
 
 
 char tela_novo_usuario (){
-    char opc;
-    system("clear||cls");
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||                                                                                                         ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||    ====================================    CADASTRAR USUÁRIO    ====================================    ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                   NOME:                                                                                 ||\n");
-    printf("||                   IDADE:                                                                                ||\n");
-    printf("||                   SEXO:                                                                                 ||\n"); 
-    printf("||                   E-MAIL:                                                                               ||\n");
-    printf("||                   PESO:                                                                                 ||\n");
-    printf("||                   ALTURA                                                                                ||\n");
-    printf("||                   FREQUÊNCIA DE ATIVIDADE FÍSICA:                                                       ||\n");
-    printf("||                   > NUNCA | RARAMENTE | FREQUENTEMENTE | TODO DIA                                       ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                   0 [Voltar]                                                                            ||\n");    
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");
-    printf("||                                                                                                         ||\n");    
-    printf("MWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM\n");
-    printf("||\n");
-    scanf(" %c", &opc);
+  char opc;
+      system("clear||cls");
+      printf("\nMWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM");
+      printf("\n||");    
+      printf("\n||");
+      printf("\n||");
+      printf("\n||    ====================================    CADASTRAR USUÁRIO    ====================================");
+      printf("\n||");
+      printf("\n||");
+      printf("\n||");
+      
+      printf("\n||                   NOME: ");    /// Nome do usuário
+      fgets(nome, sizeof(nome), stdin);
+      if (valida_nome(nome) != 0){
+        printf("\033[32m NOME VALIDO \033[0m");
+      }
+      else {
+        printf("\033[31m NOME INVALIDO! \033[0m");
+      }
+  
+      printf("\n||                   IDADE: ");   /// Idade do usuário
+      scanf("%d", &idade); getchar();
+      if (valida_idade(idade) != 0){
+        printf("\033[32m IDADE VALIDO \033[0m");
+      }
+      else {
+        printf("\033[31m IDADE INVALIDO! \033[0m");
+      }
+      
+      printf("\n||                   SEXO: ");  /// Sexo 
+      fgets(sexo, sizeof(sexo), stdin);
+      if (valida_sexo(sexo) != 0){
+        printf("\033[32m SEXO VALIDO \033[0m");
+      }
+      else {
+        printf("\033[31m SEXO INVALIDO! \033[0m");
+      }
+  
+      printf("\n||                   E-MAIL: "); /// Email
+      fgets(email, sizeof(email), stdin);
+      if (valida_email(email) != 0){
+        printf("\033[32m EMAIL VALIDO \033[0m");
+      }
+      else {
+        printf("\033[31m EMAIL INVALIDO! \033[0m");
+      }
+  
+      printf("\n||                   PESO: ");   /// Peso do usuário
+      scanf("%f", &peso); getchar();
+      if (valida_peso(peso) != 0){
+        printf("\033[32m PESO VALIDO \033[0m");
+      }
+      else {
+        printf("\033[31m PESO INVALIDO! \033[0m");
+      }
+  
+      printf("\n||                   ALTURA: "); /// Altura do usuário
+      scanf("%f", &altura); getchar();
+      if (valida_altura(altura) != 0){
+        printf("\033[32m ALTURA VALIDO \033[0m");
+      }
+      else {
+        printf("\033[31m ALTURA INVALIDO! \033[0m");
+      }
+  
+      printf("\n||                   FREQUÊNCIA DE ATIVIDADE FÍSICA:");
+      printf("\n||                   > NUNCA | RARAMENTE | FREQUENTEMENTE | TODO DIA");scanf("%s", ativ_fisica); getchar();
+      printf("\n||");
+      printf("\n||                   0 [Voltar]\n");    
+      printf("\n||");
+      printf("\n||");
+      printf("\n||");    
+      printf("\nMWMWMWMWMWMWMMWMWMWMWMMWMWMWMWMMWMWMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMMWMWMWMWMWMWMMWMWMWMMWMWMWMWMWMWMWMWMMWM");
     return opc;
 }
 
